bb5e7e8f78a6a815485861de1fb9ffddc6a9f34e
Mohammed Aarif <ali.aliaarif@gmail.com>
2022-03-12 18:52:53 +0530

    Completed Auth View

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,6 @@
 <template>
   <v-app>
-    <!-- <v-navigation-drawer app> </v-navigation-drawer> -->
-    <v-app-bar app color="indigo darken-4" dense dark v-if="auth">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Page title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+    <navbar />
     <v-main>
       <v-container class="fill-height" fluid>
         <router-view v-slot="{ Component }">
@@ -52,11 +23,14 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Navbar from "./components/Navbar.vue";
 
 export default Vue.extend({
   name: "App",
 
-  components: {},
+  components: {
+    Navbar,
+  },
 
   data: () => ({
     auth: true,
